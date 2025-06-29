e4dbd1e312d6d03a06bfe9a5279fb3ea57a59607
UdayKumarPandey <162914123+UdayKrPandey@users.noreply.github.com>
2025-06-29 12:44:17 +0530

    added Navbar and authentication page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import { Route, Routes, useLocation} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Movies from './pages/Movies.jsx'
+import MovieDetails from './pages/MovieDetails.jsx'
+import SeatLayout from './pages/SeatLayout.jsx'
+import MyBookings from './pages/MyBookings.jsx'
+import Favorite from './pages/Favorite.jsx'
+import { Toaster} from 'react-hot-toast'
+import Footer from './components/Footer.jsx'
 
 const App = () => {
+  
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
   return (
-    <div>
-          
-    </div>
+    <>
+    <Toaster />
+      { !isAdminRoute && <Navbar/>}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/movies/:id' element={<MovieDetails/>}/>
+        <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
+        <Route path='/my-bookings' element={<MyBookings/>}/>
+        <Route path='/favorite' element={<Favorite/>}/>
+      </Routes>
+      { !isAdminRoute && <Footer/>}
+    </>
   )
 }
 
